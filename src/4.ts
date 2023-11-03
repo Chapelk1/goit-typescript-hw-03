11e60f90e417ed908909975e8d106f1c9405d192
Chapelk1 <klepach154789@gmail.com>
2023-11-03 03:06:23 +0200

    +

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,55 @@
+
+
+
+
+class Key {
+    private signature: number 
+    constructor() {
+        this.signature = Math.random()
+    }
+
+    public getSignature(this: Key) {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(this:Person) {
+      return this.key
+    }
+}
+
+abstract class House {
+    protected tenants: Person[] = [];
+    protected door: boolean
+    
+    constructor(protected key: Key) {
+        this.door = false;
+    }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key:Key): void; 
+}
+
+class MyHouse extends House {
+    constructor(key:Key) {
+        super(key)
+    }
+
+    public openDoor(key: Key) {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
